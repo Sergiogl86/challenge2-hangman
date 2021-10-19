12ef99bf31a4f635392d361c3f754929fe1008d7
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-19 20:53:38 +0200

    Se completa el panel de Ahorcado y su funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Ahorcado from "./componente/Ahorcado/Ahorcado";
 
 function App() {
-  return <div className="App">Challenge2 Hangman - Sandra - Sergio</div>;
+  return (
+    <div className="App">
+      Challenge2 Hangman - Sandra - Sergio
+      <Ahorcado numeroFallos={3} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/componente/Ahorcado/Ahorcado.js
@@ -0,0 +1,145 @@
+import "./ahorcado.css";
+import { useMemo } from "react";
+
+function Ahorcado({ numeroFallos }) {
+  const stage1 = (
+    <line
+      className="stage1"
+      key="stage1"
+      x1="16"
+      y1="80"
+      x2="32"
+      y2="80"
+    ></line>
+  );
+  const stage2 = (
+    <line
+      className="stage2"
+      key="stage2"
+      x1="24"
+      y1="80"
+      x2="24"
+      y2="16"
+    ></line>
+  );
+  const stage3 = (
+    <line
+      className="stage3"
+      key="stage3"
+      x1="21"
+      y1="16"
+      x2="60"
+      y2="16"
+    ></line>
+  );
+  const stage4 = (
+    <line
+      className="stage4"
+      key="stage4"
+      x1="24"
+      y1="24"
+      x2="32"
+      y2="16"
+    ></line>
+  );
+  const stage5 = (
+    <line
+      className="stage5"
+      key="stage5"
+      x1="56"
+      y1="16"
+      x2="56"
+      y2="28"
+    ></line>
+  );
+  const stage6 = (
+    <circle
+      className="stage6"
+      key="stage6"
+      cx="56"
+      cy="34"
+      r="6"
+      fill="#bee5eb"
+    ></circle>
+  );
+  const stage7 = (
+    <line
+      className="stage7"
+      key="stage7"
+      x1="56"
+      y1="40"
+      x2="56"
+      y2="56"
+    ></line>
+  );
+  const stage8 = (
+    <line
+      className="stage8"
+      key="stage8"
+      x1="44"
+      y1="46"
+      x2="56"
+      y2="40"
+    ></line>
+  );
+  const stage9 = (
+    <line
+      className="stage9"
+      key="stage9"
+      x1="68"
+      y1="46"
+      x2="56"
+      y2="40"
+    ></line>
+  );
+  const stage10 = (
+    <line
+      className="stage10"
+      key="stage10"
+      x1="50"
+      y1="70"
+      x2="56"
+      y2="56"
+    ></line>
+  );
+  const stage11 = (
+    <line
+      className="stage11"
+      key="stage11"
+      x1="62"
+      y1="70"
+      x2="56"
+      y2="56"
+    ></line>
+  );
+
+  const arrayAhorcado = [
+    stage1,
+    stage2,
+    stage3,
+    stage4,
+    stage5,
+    stage6,
+    stage7,
+    stage8,
+    stage9,
+    stage10,
+    stage11,
+  ];
+
+  const ahorcadoSegmentosMostrar = useMemo(() => {
+    let ahorcadoSegmentos = [];
+    for (let i = 0; i < numeroFallos; i++) {
+      ahorcadoSegmentos.push(arrayAhorcado[i]);
+    }
+    return ahorcadoSegmentos;
+  }, [numeroFallos, arrayAhorcado]);
+
+  return (
+    <svg className="hangman" viewBox="0 0 96 96" width="300" height="300">
+      {ahorcadoSegmentosMostrar}
+    </svg>
+  );
+}
+
+export default Ahorcado;
