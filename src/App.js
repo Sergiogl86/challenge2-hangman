74c41a99c8d3c88dc38ea64961768f371c4220dd
Sandra Quero <sanquesan@gmail.com>
2021-10-19 21:54:29 +0200

    Creamos componente LetrasPalabras y lo instanciamos en App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import "./App.css";
 // import MensajeFinDelJuego from "./componentes/MensajeFinDelJuego/MensajeFinDelJuego";
 // import LetraFallida from "./componentes/LetraFallida/LetraFallida";
-import Ahorcado from "./componente/Ahorcado/Ahorcado";
-import MensajeFinDelJuego from "./componentes/MensajeFinDelJuego/MensajeFinDelJuego";
-import LetraFallida from "./componentes/LetraFallida/LetraFallida";
-import { useState } from "react";
-import Boton from "./componentes/Boton/Boton";
-import Imput from "./componentes/Imput/Imput";
+import Ahorcado from "./componentes/Ahorcado/Ahorcado";
+// import MensajeFinDelJuego from "./componentes/MensajeFinDelJuego/MensajeFinDelJuego";
+// import LetraFallida from "./componentes/LetraFallida/LetraFallida";
+// import { useState } from "react";
+import LetraPalabra from "./componentes/LetrasPalabras/LetrasPalabras";
+// import Boton from "./componentes/Boton/Boton";
+// import Imput from "./componentes/Imput/Imput";
 
 function App() {
   // const [letrasFalladas, setletrasFalladas] = useState(["a", "b", "c"]);
-  const [letraInput, setletraInput] = useState("");
-  function cogerValor() {
-    setletraInput(document.querySelector(".input-letra").value);
-    return letraInput;
-    // console.log(letraInput);
-  }
+  // const [letraInput, setletraInput] = useState("");
+  // function cogerValor() {
+  //   setletraInput(document.querySelector(".input-letra").value);
+  //   return letraInput;
+  //   // console.log(letraInput);
+  // }
+  const palabra = ["h", "o", "l", "a"];
 
   return (
     <div className="App">
       <section>
         <article>{/* Dibujos */}</article>
         <article>
-          {/* Componente palabra oculta */}
           <div>
-            <Imput />
-            <Boton textoBoton={"hola"} eventoClick={cogerValor} />
+            {palabra.map((letra, index) => (
+              <LetraPalabra letraPalabra={letra} key={index} />
+            ))}
+            {/* <Imput />
+            <Boton textoBoton={"hola"} eventoClick={cogerValor} /> */}
             {/* Componente Boton */}
           </div>
         </article>
         <article>{/* Componente letras Falladas */}</article>
       </section>
-    <Ahorcado numeroFallos={3} />
+      <Ahorcado numeroFallos={3} />
       {/* <LetraFallida letra={letrasFalladas} /> */}
 
       {/* <MensajeFinDelJuego mensaje={"hola"} /> */}
